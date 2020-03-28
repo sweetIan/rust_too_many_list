cf7109477cff6df3bc99cf60c44050dd69d83093
huangzhaowei <huangzhaowei@corp.netease.com>
2020-03-28 18:42:03 +0800

    implement a bad stack

---
+++ src/first.rs
@@ -0,0 +1,94 @@
+use std::mem;
+use std::io::Empty;
+
+pub struct List {
+    head: Link,
+}
+
+struct Node {
+    elem: i32,
+    next: Link,
+}
+
+enum Link {
+    Empty,
+    More(Box<Node>),
+}
+
+impl List {
+    pub fn new() -> Self {
+        List { head: Link::Empty }
+    }
+
+    pub fn push(&mut self, val: i32) {
+        let new_node = Box::new(Node {
+            elem: val,
+            next: mem::replace(&mut self.head, Link::Empty),
+        });
+        self.head = Link::More(new_node);
+    }
+
+    pub fn push2(mut self, val: i32) -> Self {
+        let next = mem::replace(&mut self.head, Link::Empty);
+        let new_node = Box::new(Node {
+            elem: val,
+            next,
+        });
+        List { head: Link::More(new_node) }
+    }
+
+    pub fn pop(&mut self) -> Option<i32> {
+        match self.pop_node() {
+            Link::Empty => None,
+            Link::More(node) => {
+                self.head = node.next;
+                Some(node.elem)
+            }
+        }
+    }
+
+    fn pop_node(&mut self) -> Link {
+        mem::replace(&mut self.head, Link::Empty)
+    }
+}
+
+impl Drop for List {
+    fn drop(&mut self) {
+        let mut cur = self;
+        while let Link::More(cur) = cur.pop_node() {}
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+
+        // Check empty list behaves right
+        assert_eq!(list.pop(), None);
+
+        // Populate list
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        // Push some more just to make sure nothing's corrupted
+        list.push(4);
+        list = list.push2(5);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+
+        // Check exhaustion
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
+    }
+}
